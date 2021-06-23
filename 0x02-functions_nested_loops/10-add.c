6c0c5be2c64cf12adac7f6df4abc27fdb199a22a
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-23 13:20:43 -0700

    Print add operation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* add - entry point
+* @n: number to validate
+* @m: number to validate
+*
+* Description: Prints the result of adding two numbers
+*
+* Return: void
+*/
+
+int add(int n, int m)
+{
+	return (n + m);
+}
