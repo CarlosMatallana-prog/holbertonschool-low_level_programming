3dd5ab4bebca02c6a671f813b636003c8e0d0b83
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-23 12:21:20 -0700

    Fix output

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,30 +11,28 @@
 void times_table(void)
 {
 
-	int c, i, j, d, u, space, coma;
+	int i, j, d, u, space, coma;
 
-	j = 0;
-	c = 0;
+	j = 0;	
 	space = 32;
 	coma = 44;
 
-	while (c <= 9)
+	while (j <= 9)
 	{
-		while (j <= 9)
+		for (i = 0; i <= 9; ++i)
 		{
-			for (i = 0; i <= 9; ++i)
+			d = ((j * i) / 10) + 48;
+			u = ((j * i) % 10) + 48;
+			(j * i > 9) ? _putchar(d) : _putchar(space);
+			_putchar(u);
+			if (i < 9)
 			{
-				d = ((j * i) / 10) + 48;
-				u = ((j * i) % 10) + 48;
-				(j * i > 9) ? _putchar(d) : _putchar(space);
-				_putchar(u);
-				if (i < 9)
-					_putchar(coma);
+				_putchar(coma);		       
 				_putchar(space);
 			}
-			_putchar('\n');
-			j++;
 		}
-		c++;
+		_putchar('\n');
+		j++;
 	}
+
 }
