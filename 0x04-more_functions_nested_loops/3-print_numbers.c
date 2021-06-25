c570a9602c554f6eb43524f52b021c87dbd6ce14
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-24 18:05:23 -0700

    Print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_numbers - prints all digits
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + 48);
+		i++;
+	}
+	_putchar('\n');
+}
