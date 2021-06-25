2ef14090cfb9cd0449058ab9f1b334ade3fad14f
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-25 10:04:26 -0700

    10 times the number from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* more_numbers - prints all digits
+*
+* Return: void
+*/
+void more_numbers(void)
+{
+	int times = 0, number, tens, units;
+
+	while (times <= 10)
+	{
+		for (number = 0; number <= 14; ++number)
+		{
+			units = number % 10;
+			if (number > 9)
+				tens = (number / 10) + '0';
+			else
+				tens = 0;
+			_putchar(tens);
+			_putchar(units + '0');
+		}
+		_putchar('\n');
+		times++;
+	}
+}
