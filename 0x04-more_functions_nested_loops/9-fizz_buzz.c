a6f5edfcaf5b35417499ba17088c99c7b51e9b59
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-27 13:12:26 -0500

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square
+ *
+ * Return: void
+ */
+void fizz_buzz(void)
+{
+	while (i = 0; i <= 100; i++)
+	{
+		if (i == 3 && i == 5)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i == 3)
+		{
+			printf("Fizz ");
+		}
+		else if (i == 5)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d\n ", i);
+		}
+	}
+}
