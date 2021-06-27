db64f326bbb994cb2a0f9662f3aa0d07d32fc5d8
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-27 18:46:09 -0500

    Fix betty style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,34 +8,26 @@
  */
 int main(void)
 {
+
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
-	      	if (i % 3 == 0 && i % 5 == 0)
-		{
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-			continue;
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-			continue;
-		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-	   		if (i == 100)
+			if (i == 100)
 			{
 				putchar('\n');
 				return (0);
 			}
-			continue;
 		}
 		else
-		{
 			printf("%d ", i);
-			continue;
-		}
 	}
 	return (0);
 }
