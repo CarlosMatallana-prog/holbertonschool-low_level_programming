4df61314d0f30eb3911c71fd59e92be6112312ca
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-27 14:55:39 -0700

    Add function 10 file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: Size of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int rows = 1, columns;
+
+	if (size > 0)
+	{
+		while (rows <= size)
+		{
+			columns = 1;
+			while (columns <= size)
+			{
+				_putchar(35);
+				columns++;
+			}
+			_putchar('\n');
+			rows++;
+		}
+	}
+	else
+		_putchar('\n');
+}
