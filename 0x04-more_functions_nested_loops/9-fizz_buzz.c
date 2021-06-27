27d225ada6673d3be7ecbaafb296ab540637d056
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-27 18:39:32 -0500

    Fix function definition

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,30 +2,40 @@
 #include <stdio.h>
 
 /**
- * fizz_buzz - Prints FizzBuzz
+ * main - Prints FizzBuzz
  *
- * Return: void
+ * Return: 0, success
  */
-void fizz_buzz(void)
+int main(void)
 {
 	int i;
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 	      	if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
+			continue;
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
+			continue;
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
+	   		if (i == 100)
+			{
+				putchar('\n');
+				return (0);
+			}
+			continue;
 		}
 		else
 		{
 			printf("%d ", i);
+			continue;
 		}
 	}
+	return (0);
 }
