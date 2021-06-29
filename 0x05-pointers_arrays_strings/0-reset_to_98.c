f3b12022baa61eb8d7687a1f382ab25e5c632369
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-29 14:39:38 -0500

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Prints FizzBuzz
+ * @n: the pointer with the value
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
