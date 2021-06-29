278c737ffed38b02d745aac1669dd9647fe1059c
CarlosMatallana-prog <3496@holbertonschool.com>
2021-06-29 18:25:38 -0500

    Custon puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts - return the lenght of a string
+ * @str: the string pointer
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
