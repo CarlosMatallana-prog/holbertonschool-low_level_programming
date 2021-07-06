3fa0824fbfb7ff2dfa3c71e220f5b8691d943f15
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-06 11:23:22 -0500

    Check a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: the string to check
+ * @c: the charcter to check
+ *
+ * Return: a pointer to c if true, or null if false
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return ('\0');
+}
