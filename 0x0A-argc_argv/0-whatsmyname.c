9229519be6032f0095e37e1c5eda8ee6722425c0
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-08 13:35:36 -0500

    Print the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * @argc: unused argument
+ * @argv: argument vector
+ *
+ * Return: success status
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
