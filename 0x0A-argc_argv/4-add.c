2a6c73e4987cacfa62794ebb979acf3e7575fadd
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-09 17:12:46 -0500

    Add numbers to the infinite and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,36 +10,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int numbers = argc - 1, digit = 0, iterator, result = 0, is_digit = 0;
+	int iterator, result = 0, iterator2;
+	int numbers = argc - 1;
 
 	if (numbers >= 1)
 	{
 		for (iterator = 1; iterator <= numbers; iterator++)
 		{
-			digit = (atoi(argv[iterator]));
-			is_digit = digit == 0 ? 0 : 1;
-			if (is_digit == 1)
+			for (iterator2 = 0; argv[iterator][iterator2]  != '\0'; iterator2++)
 			{
-				result += digit;
+				if (argv[iterator][iterator2] < 48 || argv[iterator][iterator2] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				/* If is not a digit, print Error, new line, and return 1 */
-				printf("Error\n");
-				return (1);
-			}
-
+			result += (atoi(argv[iterator]));
 		}
-		/* If it is a digit print the result, and return 0 */
 		printf("%i\n", result);
 		return (0);
 	}
 	else
-	{
-		/* If no number is passed to the program, print 0, followed by a new line */
 		printf("0\n");
-	}
 
 	return (1);
-
 }
