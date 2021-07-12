5ef184bf3d85c1ec2244f01d30025211618a409e
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-12 17:12:34 -0500

    _print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Revers and print a string
+ *
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		s++;
+		_puts_recursion(s);
+		putchar(s[0]);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
