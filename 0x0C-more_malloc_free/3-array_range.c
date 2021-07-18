e638e4bb84a785e271905e856f804e6136bbc56e
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-18 18:21:47 -0500

    Aunction that creates an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * array_range -  creates an array of integers
+ *
+ * @min: the minimun value of the array
+ * @max: the max value of the array
+ *
+ * Return: the pointer to the newly created array || NULL if fails
+ */
+int *array_range(int min, int max)
+{
+	int *chain, i, j;
+
+	if (min > max)
+		return (NULL);
+
+	chain = malloc(sizeof(int) * (max - min) + 1);
+	if (chain != NULL)
+	{
+		j = min;
+		for (i = 0; i < (max - min) + 1; i++)
+		{
+			chain[i] = j;
+			j++;
+		}
+		return (chain);
+	}
+	return (NULL);
+
+}
