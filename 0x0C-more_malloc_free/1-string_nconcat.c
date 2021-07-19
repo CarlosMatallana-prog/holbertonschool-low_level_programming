2243cd84d205dd506776a24965d89e9940d7aebb
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-18 21:07:03 -0500

    Fix output error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,9 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (j = 0; i < k; i++, j++)
 			str[i] = s2[j];
 		str[i] = '\0';
-		return (str);
-
 	}
-	return NULL;
+	return (str);
 
 }
