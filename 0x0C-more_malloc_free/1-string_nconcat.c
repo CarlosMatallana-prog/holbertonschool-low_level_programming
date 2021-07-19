af07165e173e8c4973aef298b10007eb3c473714
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-18 21:04:05 -0500

    Fix output error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j, k;
 
-	if (s1 == NULL || s2 == NULL || n <= 0)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	if (n <= 0)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
@@ -22,21 +28,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; s2[j] != '\0'; j++)
 		continue;
 
-	if (n > j)
-		return (NULL);
+	if (n >= j)
+		n = j;
 
 	k = i + n;
 
 	str = malloc((k + 1) * sizeof(char));
 
-	if (str == NULL)
-		return (NULL);
+	if (str != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			str[i] = s1[i];
+		for (j = 0; i < k; i++, j++)
+			str[i] = s2[j];
+		str[i] = '\0';
+		return (str);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
-	for (j = 0; i < k; i++, j++)
-		str[i] = s2[j];
-	str[i] = '\0';
+	}
+	return NULL;
 
-	return (str);
 }
