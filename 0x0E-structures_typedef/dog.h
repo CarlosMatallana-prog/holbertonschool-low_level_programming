19c5d528b508d66bcc40036d96794399f64b36d5
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-19 16:54:33 -0500

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - structure of the dog.
+ *
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Name of the owner.
+ *
+ * Description: Structure for storing the params of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* DOG */
