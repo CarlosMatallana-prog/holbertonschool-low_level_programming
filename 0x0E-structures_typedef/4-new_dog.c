b61f0a32c2dfe95ee8f3747c27c1581652abf497
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-20 15:57:08 -0500

    Fix Betty style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,14 +18,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int name_size = 0, owner_size = 0, k, l;
 	dog_t *new_puppy;
 
-	if (name == 0 || owner == 0)
-		return (NULL);
-
 	new_puppy = malloc(sizeof(dog_t));
 	if (new_puppy == NULL)
 		return (NULL);
-
-	/* store the dog name */
 	while (name[name_size] != '\0')
 		name_size++;
 	store_name = malloc((name_size * sizeof(char)) + 1);
@@ -36,8 +31,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (k = 0; k < name_size + 1; ++k)
 		store_name[k] = name[k];
-
-	/* store the dog owner */
 	while (owner[owner_size] != '\0')
 		owner_size++;
 	store_owner = malloc((owner_size * sizeof(char)) + 1);
@@ -49,12 +42,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (l = 0; l < owner_size + 1; ++l)
 		store_owner[l] = owner[l];
-
 	new_puppy->name = store_name;
 	new_puppy->age = age;
 	new_puppy->owner = store_owner;
-
 	return (new_puppy);
 
 }
-
