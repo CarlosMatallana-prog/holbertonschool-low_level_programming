c972dbb78acee783a45bd80c451f0647979b9561
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-21 12:15:52 -0500

    Update header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,16 +1,8 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
-/**
- * struct dog - structure of the dog.
- *
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Name of the owner.
- *
- * Description: Structure for storing the params of the dog.
- */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
-#endif /* DOG */
+#endif /* FUNCTION_POINTERS */
