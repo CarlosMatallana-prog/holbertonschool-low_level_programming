af3c78215fc581ab3d98229dc8f6c801228ca1cf
CarlosMatallana-prog <3496@holbertonschool.com>
2021-07-23 13:50:40 -0700

    Fix, reestructure separator validation

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,15 +22,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(struc, int));
-			if (i < (n - 1))
-			{
-				if (separator == NULL)
-					printf("%s", "");
-
-				else
-					printf("%s", separator);
-
-			}
+			if (separator != NULL && i < (n - 1))
+				printf("%s", separator);
 		}
 		printf("\n");
 	}
