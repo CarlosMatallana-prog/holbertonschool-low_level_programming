bac628a6484d2e85c3d04235baf169b020dd29e7
CarlosMatallana-prog <3496@holbertonschool.com>
2021-08-09 18:10:08 -0500

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL || filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDWR | O_CREAT | O_TRUN, 0600);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
+
+	while (text_content[len])
+	{
+		len++;
+	}
 
 	print = write(file, text_content, len);
 	if (print == -1)
