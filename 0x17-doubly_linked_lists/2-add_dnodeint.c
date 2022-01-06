2539bab08214de17d5e55cef3a613dc5074cab6c
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-06 16:07:25 -0500

    :art: Improve code estructure

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,26 +1,1 @@
-#include "lists.h"
-
-/**
- * add_dnodeint - adds a new node at the end of a dlistint_t list.
- * @head: The address of the head
- * @n: the data to fill the new node in the list
- *
- * Return: the new node
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-    dlistint_t *new;
-
-    if (head == NULL)
-        return (NULL);
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = n;
-    new->next = *head;
-    new->prev = NULL;
-    *head = new;
-    if (new->next != NULL)
-        (new->next)->prev = new;
-    return (new);
-}
+#include "lists.h"/** * add_dnodeint - adds a new node at the end of a dlistint_t list. * @head: The address of the head * @n: the data to fill the new node in the list * * Return: the new node */dlistint_t *add_dnodeint(dlistint_t **head, const int n) {    dlistint_t *new;    if (head == NULL)        return (NULL);    new = malloc(sizeof(dlistint_t));    if (new == NULL)        return (NULL);    new->n = n;    new->next = *head;    new->prev = NULL;    *head = new;    if (new->next != NULL)        (new->next)->prev = new;    return (new);}
