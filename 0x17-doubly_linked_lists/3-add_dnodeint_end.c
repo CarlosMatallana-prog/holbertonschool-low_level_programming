4d8b214dbc2ecbc79559f052af87679f4120c432
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-06 15:55:42 -0500

    :art: Improve code estructure

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_t - adds a new node at the end of a dlistint_t list.
+ * @h: the list
+ *
+ * Return: the number of nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n) {
+    dlistint_t *new;
+
+    if (head == NULL)
+        return (NULL);
+
+    new = malloc(sizeof(dlistint_t));
+
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->prev = NULL;
+    new->next = *head;
+    *head = new;
+    if (new->next != NULL)
+        (new->next)->prev = new;
+    return (new);
+}
