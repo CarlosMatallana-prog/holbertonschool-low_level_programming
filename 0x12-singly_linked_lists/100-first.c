2d74863b41ccd81cfcd45ae4767563512c78477f
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-11 16:31:58 -0500

    :bug: Fix structure

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 
-void before() __attribute__ ((constructor));
+
+void before(void) __attribute__ ((constructor));
 
 /**
  * before - a function that prints a phrase before main
@@ -9,6 +10,6 @@ void before() __attribute__ ((constructor));
  */
 void before(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
