cb2dbecbbd82b9af419a5885bec38b9fcf1c2921
CarlosMatallana-prog <3496@holbertonschool.com>
2022-02-02 14:05:35 -0500

    :bug: Fix, add missing }

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,8 +19,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 
 	if (ht->array[index])
+	{
 		tmp = ht->array[index];
 		while (tmp)
+		{
 			if (strcmp(tmp->key, key) == 0)
 			{
 				free(tmp->value);
