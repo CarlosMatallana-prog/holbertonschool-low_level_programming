e80ea1dd928e5715ea3a6b087b1b1a5775259b19
CarlosMatallana-prog <3496@holbertonschool.com>
2022-06-12 23:11:47 +0000

    :hammer: Implement the Binary search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,9 @@
 #include <math.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_helper(int *array, int value,
+						 size_t left_idx, size_t right_idx);
+void print_subarray(int *array, size_t left_idx, size_t right_idx);
 
 #endif
