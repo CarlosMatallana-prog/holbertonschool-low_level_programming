ea9f5da9ab5526a377164732b51b1ba53affc95a
CarlosMatallana-prog <3496@holbertonschool.com>
2022-06-13 13:04:29 +0000

    :hammer: searches for a value in a sorted array of integers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ int binary_search(int *array, size_t size, int value);
 int binary_search_helper(int *array, int value,
 						 size_t left_idx, size_t right_idx);
 void print_subarray(int *array, size_t left_idx, size_t right_idx);
+int jump_search(int *array, size_t size, int value);
 
 #endif
